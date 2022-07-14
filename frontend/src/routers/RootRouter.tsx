ff8824cb3a6413bee3f813ef9f7174a34dca12d4
Yeom-JinHo <pwlsghq@naver.com>
2022-07-14 22:59:12 +0900

    feat : 회원가입 첫 페이지 #4

--- frontend/src/routers/RootRouter.tsx
+++ frontend/src/routers/RootRouter.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import TipPage from "@screens/TipPage";
 import DealPage from "@screens/DealPage";
+import Join from "@screens/Join/Join";
 
 function RootRouter() {
   return (
@@ -15,6 +16,7 @@ function RootRouter() {
         <Route path="/tip" element={<TipPage />} />
         <Route path="/feed" element={<FeedPage />} />
         <Route path="/deal" element={<DealPage />} />
+        <Route path="/join" element={<Join />} />
       </Routes>
     </>
   );
