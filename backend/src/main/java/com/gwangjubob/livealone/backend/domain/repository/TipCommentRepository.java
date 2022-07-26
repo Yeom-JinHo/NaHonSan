f6fc20531138d2f8e65d232e8338bac5c58159de
hyejin-github <hyejineeeeee@gmail.com>
2022-07-26 23:39:07 +0900

    feat : 꿀팁 댓글,대댓글 수정 API 구현 #33

--- backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/TipCommentRepository.java
+++ backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/TipCommentRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TipCommentRepository extends JpaRepository<TipCommentEntity, Integer> {
     TipCommentEntity findByIdx(Integer idx);
+
+    void deleteByIdx(Integer idx);
 }
