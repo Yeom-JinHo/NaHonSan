fd6471e820ad079bd353c2dc708e4bbe86e7a923
Yeom-JinHo <pwlsghq@naver.com>
2022-07-27 21:59:01 +0900

    feat : DM 채팅방 역 무한스크롤  #46

--- frontend/src/screens/ChatRoom.tsx
+++ frontend/src/screens/ChatRoom.tsx
@@ -1,34 +1,66 @@
 import Chat, { ChatProps } from "@components/Letters/Chat";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { v4 } from "uuid";
 import "./ChatRoom.scss";
 import UserDummyIcon from "@images/UserDummy.svg";
 import ImgIcon from "@images/ImgIcon.svg";
 import { getDmDetailList } from "@apis/dm";
+import loadingSpinner from "@images/LoadingSpinner.svg";
 
 function ChatRoom() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const withId = searchParams.get("with");
-  const [dmList, setDmList] = useState<any>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [dmList, setDmList] = useState<Array<ChatProps>>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [firstLoading, setFirstLoading] = useState(false);
   const [page, setPage] = useState(0);
+  const observerTarget = useRef<HTMLDivElement>(null);
 
+  const getDmList = async () => {
+    setIsLoading(true);
+    const res = await getDmDetailList(withId as string);
+    if (res.message === "SUCCESS") {
+      setFirstLoading(true);
+      const dummyTest = res.data.map((dm: ChatProps) => ({
+        ...dm,
+        content: dm.content + page
+      }));
+      setDmList([...dmList, ...dummyTest]);
+    }
+    setIsLoading(false);
+  };
   useEffect(() => {
     if (!withId) {
       navigate("/404");
-    } else {
-      (async () => {
-        const res = await getDmDetailList(withId);
-        if (res.message === "SUCCESS") {
-          setIsLoading(false);
-          setDmList(res.data);
-        }
-      })();
     }
   }, []);
+
+  useEffect(() => {
+    getDmList();
+  }, [page]);
+
+  const onIntersect = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      if (entry.isIntersecting && !isLoading) {
+        setPage(prev => prev + 1);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersect, { threshold: 0.1 });
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [dmList]);
+
   return (
     <div className="wrapper">
       <div id="chat-room">
@@ -42,12 +74,31 @@ function ChatRoom() {
         </header>
         <div className="chat-list">
           {dmList.length !== 0 ? (
-            dmList.map((dm: ChatProps) => (
-              <Chat type={dm.type} content={dm.content} key={v4()} />
-            ))
+            <>
+              {dmList.map((dm: ChatProps) => (
+                <Chat type={dm.type} content={dm.content} key={v4()} />
+              ))}
+              {isLoading ? (
+                <div
+                  className="spinner-wrraper
+                flex justify-center"
+                >
+                  <img
+                    src={loadingSpinner}
+                    title="로딩스피너"
+                    alt="로딩스피너"
+                    className="loading-spinner"
+                  />
+                </div>
+              ) : (
+                <div ref={observerTarget} className="observerTarget">
+                  여기!
+                </div>
+              )}
+            </>
           ) : (
             <div className="no-chat flex align-center justify-center fs-24">
-              {!isLoading && "주고받은 대화가 없습니다!"}
+              {firstLoading && "주고받은 대화가 없습니다!"}
             </div>
           )}
         </div>
