7a905b31e9150142b7829e9a988516834ca8ad89
stopone2639 <stopone2639@naver.com>
2022-07-29 17:53:06 +0900

    test : 꿀딜 좋아요 #34
    꿀딜 좋아요

--- backend/src/test/java/com/gwangjubob/livealone/backend/service/UserFeedServiceTest.java
+++ backend/src/test/java/com/gwangjubob/livealone/backend/service/UserFeedServiceTest.java
@@ -9,7 +9,6 @@ import com.gwangjubob.livealone.backend.mapper.DealMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
