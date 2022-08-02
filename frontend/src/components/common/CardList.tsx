e97216bcac452352abc86968b9274004a91270f5
Yeom-JinHo <pwlsghq@naver.com>
2022-08-02 21:45:49 +0900

    feat : 꿀딜 무한스크롤 #58

--- frontend/src/components/common/CardList.tsx
+++ frontend/src/components/common/CardList.tsx
@@ -1,31 +1,78 @@
 import { getDummy } from "@apis/dummy";
+import { getTipList } from "@apis/tip";
 import React, { useEffect, useState } from "react";
 import { v4 } from "uuid";
-import Card from "./Card";
+import Card, { CardProps } from "./Card";
 import "./CardList.scss";
 import CardSkeleton from "./CardSkeleton";
 
-type CardListProps = {
-  type: "tip" | "deal";
+export type conditionType = {
+  sort: string;
+  searchType: "deal" | "tip";
+  keyword: string | null;
+  category: string;
+  page: number;
 };
 
-function CardList({ type }: CardListProps) {
-  const [dummy, setDummy] = useState<Array<any>>([]);
+type CardListProps = conditionType & {
+  handleSpinner: () => void;
+  handleIsEnd: () => void;
+};
+
+function CardList({
+  searchType,
+  sort,
+  keyword,
+  category,
+  page,
+  handleSpinner,
+  handleIsEnd
+}: CardListProps) {
+  const [cards, setCards] = useState<Array<CardProps>>([]);
   const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     (async () => {
-      const res = await getDummy();
-      setDummy(res);
-      setIsLoading(true);
+      if (searchType === "tip") {
+        const res = await getTipList(category, keyword, sort, page);
+        if (res.isEnd) handleIsEnd();
+        setCards(res.data);
+        setIsLoading(true);
+        handleSpinner();
+      }
     })();
   }, []);
   return (
     <div id="card-list" className="flex justify-center">
       {isLoading
-        ? dummy.map(value => <Card type={type} data={value} key={v4()} />)
+        ? cards.map(
+            ({
+              type,
+              idx,
+              userNickname,
+              userProfileImg,
+              title,
+              bannerImg,
+              like,
+              comment,
+              view
+            }) => (
+              <Card
+                type={type}
+                idx={idx}
+                userNickname={userNickname}
+                userProfileImg={userProfileImg}
+                title={title}
+                bannerImg={bannerImg}
+                like={like}
+                comment={comment}
+                view={view}
+                key={v4()}
+              />
+            )
+          )
         : [0, 1, 2, 3, 4, 5].map(() => <CardSkeleton key={v4()} />)}
     </div>
   );
 }
 
-export default CardList;
+export default React.memo(CardList);
